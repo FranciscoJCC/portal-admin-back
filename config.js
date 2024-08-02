6bb13b07de42f6e67f50f258e94db2d8a08d95e4
Francisco Cervantes <developer1@highpower.com.mx>
2024-08-02 15:29:53 -0600

    Instalacion y configuracion de sequelize

--- config.js
+++ config.js
@@ -9,4 +9,9 @@ module.exports = {
     NODE_ENV: process.env.NODE_ENV || 'dev',
 
     API_PORT: process.env.API_PORT || 8098,
+    MYSQL_USER: process.env.MYSQL_USER || '',
+    MYSQL_PASSWORD: process.env.MYSQL_PASSWORD || '',
+    MYSQL_HOST: process.env.MYSQL_HOST || '',
+    MYSQL_PORT: process.env.MYSQL_PORT || '',
+    MYSQL_DB_ACTUAR : process.env.MYSQL_DB_ACTUAR || ''
 }
